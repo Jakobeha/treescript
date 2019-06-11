a9c4d140f42184fb54f5e67642a39a7bb3699d79
jakob <jakobeha@gmail.com>
2019-06-11 17:50:45 -0400

    ...

--- src/TreeScript/Ast/Flat/Parse.hs
+++ src/TreeScript/Ast/Flat/Parse.hs
@@ -2,62 +2,73 @@
 
 -- | Parse lexemes from AST data.
 module TreeScript.Ast.Flat.Parse
-  ( parse
-  ) where
+  ( parseList
+  , parseStream
+  )
+where
 
-import TreeScript.Ast.Flat.Types
-import TreeScript.Misc
+import           TreeScript.Ast.Flat.Types
+import           TreeScript.Misc
+import qualified TreeScript.Misc.Ext.Attoparsec
+                                               as P
 
-import Data.Char
-import qualified Data.Text as T
-import Data.Void
-import qualified Text.Megaparsec as P
-import qualified Text.Megaparsec.Char as P
-import qualified Text.Megaparsec.Char.Lexer as P
+import           Control.Applicative
+import           Control.Monad
+import           Data.Char
+import qualified Data.Text                     as T
+import qualified Data.Attoparsec.Text          as P
+import qualified System.IO.Streams             as S
+import qualified System.IO.Streams.Attoparsec.Text
+                                               as S
 
 mkError :: T.Text -> Error
-mkError msg
-  = Error
-  { errorStage = StageLex
-  , errorRange = r0
-  , errorMsg = msg
-  }
+mkError msg = Error { errorStage = StageLex, errorRange = r0, errorMsg = msg }
 
-parse :: T.Text -> Result [[Lexeme]]
-parse txt = traverse parse1 $ filter (not . T.null) $ T.lines txt
-  where
-    parse1 txt1
-      = case P.runParser astDataParser "" txt1 of
-          Left err -> mkFail $ mkError $ T.pack $ P.errorBundlePretty (err :: P.ParseErrorBundle T.Text Void)
-          Right res -> pure res
-    astDataParser = P.many lexemeParser <* P.eof
-    lexemeParser = do
-      word <- wordParser
-      case word of
-        "splice" -> do
-          idx <- P.decimal
-          separatorParser
-          pure $ LexemeEnterSplice idx
-        "integer" -> do
-          value <- P.decimal
-          separatorParser
-          pure $ LexemePrimitive $ PrimInteger value
-        "float" -> do
-          value <- P.float
-          separatorParser
-          pure $ LexemePrimitive $ PrimFloat value
-        "string" -> do
-          value <- stringParser
-          separatorParser
-          pure $ LexemePrimitive $ PrimString value
-        _ | isUpper $ head word -> do
-              numProps <- P.decimal
-              separatorParser
-              pure $ LexemeRecordHead (T.pack word) numProps
-          | otherwise -> fail $ "word has unknown type: " ++ word
-    stringParser = T.pack <$> (P.char '"' *> P.manyTill P.charLiteral (P.char '"'))
-    wordParser = do
-      res <- P.some (P.anySingleBut ' ')
+separatorParser :: P.Parser ()
+separatorParser = (() <$ P.char ' ') <|> P.endOfInput
+
+wordParser :: P.Parser String
+wordParser = do
+  res <- some (P.notChar ' ')
+  separatorParser
+  pure res
+
+lexemeParser :: P.Parser Lexeme
+lexemeParser = do
+  word <- wordParser
+  case word of
+    "splice" -> do
+      idx <- P.decimal
       separatorParser
-      pure res
-    separatorParser = (() <$ P.char ' ') P.<|> P.eof
+      pure $ LexemeEnterSplice idx
+    "integer" -> do
+      value <- P.decimal
+      separatorParser
+      pure $ LexemePrimitive $ PrimInteger value
+    "float" -> do
+      value <- realToFrac <$> P.double
+      separatorParser
+      pure $ LexemePrimitive $ PrimFloat value
+    "string" -> do
+      value <- P.stringLiteral
+      separatorParser
+      pure $ LexemePrimitive $ PrimString value
+    _
+      | isUpper $ head word -> do
+        numProps <- P.decimal
+        separatorParser
+        pure $ LexemeRecordHead (T.pack word) numProps
+      | otherwise -> fail $ "word has unknown type: " ++ word
+
+astParser :: P.Parser [Lexeme]
+astParser = lexemeParser `P.manyTill` ((() <$ P.endOfLine) <|> P.endOfInput)
+
+parseList :: T.Text -> Result [[Lexeme]]
+parseList txt = case P.parseOnly parser txt of
+  Left  err -> mkFail $ mkError $ T.pack err
+  Right xs  -> pure $ filter (not . null) xs
+  where parser = many astParser <* P.endOfInput
+
+parseStream :: S.InputStream T.Text -> IO (S.InputStream [Lexeme])
+parseStream = S.filter (not . null) <=< S.parserToInputStream parser
+  where parser = (Nothing <$ P.endOfInput) <|> (Just <$> astParser)
--- src/TreeScript/Plugin/CmdProgram.hs
+++ src/TreeScript/Plugin/CmdProgram.hs
@@ -1,24 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLists #-}
 
 -- | Allows the TreeScript compiler to use external command-line programs.
 module TreeScript.Plugin.CmdProgram
-  ( CmdProgram (..)
+  ( CmdProgram(..)
+  , runCmdProgramStreamArgs
   , runCmdProgramArgs
+  , runCmdProgramStream
   , runCmdProgram
-  ) where
+  )
+where
 
-import TreeScript.Misc
+import           TreeScript.Plugin.ResultInputStream
+import           TreeScript.Misc
 
-import Control.Monad
-import Control.Monad.Catch
-import Control.Monad.Fail
-import Control.Monad.IO.Class
-import qualified Data.Text as T
-import qualified Data.Text.IO as T
-import System.Environment
-import System.Exit
-import System.IO
-import System.Process
+import           Control.Monad
+import           Control.Monad.Catch
+import           Control.Monad.Fail
+import           Control.Monad.IO.Class
+import qualified Data.ByteString               as B
+import qualified Data.Text                     as T
+import qualified Data.Text.IO                  as T
+import           System.Environment
+import           System.Exit
+import           System.IO
+import qualified System.IO.Streams             as S
+import           System.Process
 
 -- | Represents an external command-line program.
 data CmdProgram
@@ -29,44 +36,89 @@ data CmdProgram
   } deriving (Read, Show)
 
 convertErr :: Stage -> T.Text -> Error
-convertErr stage err
-  = Error
-    { errorStage = stage
-    , errorRange = r0
-    , errorMsg = err
-    }
+convertErr stage err =
+  Error { errorStage = stage, errorRange = r0, errorMsg = err }
+
+-- | Runs the command-line program, passing the stream to stdin, and returning a stream from stdout.
+runCmdProgramStreamArgs
+  :: (MonadIO m, MonadCatch m, MonadFail m, MonadResult m)
+  => CmdProgram
+  -> [T.Text]
+  -> S.OutputStream B.ByteString
+  -> m (ResultInputStream T.Text)
+runCmdProgramStreamArgs (CmdProgram stage ppath progEnv) args inp = do
+  let liftCmdIO = liftIOAndCatch stage
+  parentEnv <- liftCmdIO getEnvironment
+  pin       <- liftCmdIO $ S.outputStreamToHandle inp
+  let pproc = (proc ppath $ map T.unpack args) { env     = Just
+                                                 $  parentEnv
+                                                 ++ progEnv
+                                               , std_in  = UseHandle pin
+                                               , std_out = CreatePipe
+                                               , std_err = CreatePipe
+                                               }
+  (Nothing, Just pout, Just perr, phandle) <- liftCmdIO $ createProcess pproc
+  -- Not sure why but not setting these encodings sometimes causes an "invalid byte sequence" error
+  liftCmdIO $ hSetEncoding pout latin1
+  liftCmdIO $ hSetEncoding perr latin1
+  out <-
+    liftCmdIO
+    $   S.map (Result [])
+    =<< S.decodeUtf8
+    =<< S.handleToInputStream pout
+  pure $ ResultInputStream out perr phandle
 
 -- | Runs the command-line program, passing the text to stdin, and returning stdout.
-runCmdProgramArgs :: (MonadIO m, MonadCatch m, MonadFail m, MonadResult m) => CmdProgram -> [T.Text] -> T.Text -> m T.Text
+runCmdProgramArgs
+  :: (MonadIO m, MonadCatch m, MonadFail m, MonadResult m)
+  => CmdProgram
+  -> [T.Text]
+  -> T.Text
+  -> m T.Text
 runCmdProgramArgs (CmdProgram stage ppath progEnv) args inp = do
   let liftCmdIO = liftIOAndCatch stage
   parentEnv <- liftCmdIO getEnvironment
-  let pproc
-        = (proc ppath $ map T.unpack args)
-        { env = Just $ parentEnv ++ progEnv
-        , std_in = CreatePipe
-        , std_out = CreatePipe
-        , std_err = CreatePipe
-        }
+  let pproc = (proc ppath $ map T.unpack args) { env     = Just
+                                                 $  parentEnv
+                                                 ++ progEnv
+                                               , std_in  = CreatePipe
+                                               , std_out = CreatePipe
+                                               , std_err = CreatePipe
+                                               }
   (Just pin, Just pout, Just perr, phandle) <- liftCmdIO $ createProcess pproc
   -- Not sure why but not setting these encodings sometimes causes an "invalid byte sequence" error
   liftCmdIO $ hSetEncoding pout latin1
   liftCmdIO $ hSetEncoding perr latin1
   liftCmdIO $ T.hPutStr pin inp
   liftCmdIO $ hClose pin
-  err <- liftCmdIO $ redirectExceptLast perr
+  err      <- liftCmdIO $ redirectExceptLast perr
   exitCode <- liftCmdIO $ waitForProcess phandle
   case exitCode of
     ExitSuccess -> do
-      unless (T.null $ T.strip err) $
-        liftCmdIO $ T.putStrLn err
+      unless (T.null $ T.strip err) $ liftCmdIO $ T.putStrLn err
       liftCmdIO $ T.hGetContents pout
     ExitFailure code
-      | T.null $ T.strip err ->
-          mkFail $ convertErr stage $ "unknown error (code " <> pprint code <> ")"
-      | otherwise ->
-          mkFail $ convertErr stage $ T.strip err
+      | T.null $ T.strip err
+      -> mkFail
+        $  convertErr stage
+        $  "unknown error (code "
+        <> pprint code
+        <> ")"
+      | otherwise
+      -> mkFail $ convertErr stage $ T.strip err
 
 -- | Runs the command-line program without arguments.
-runCmdProgram :: (MonadIO m, MonadCatch m, MonadFail m, MonadResult m) => CmdProgram -> T.Text -> m T.Text
+runCmdProgram
+  :: (MonadIO m, MonadCatch m, MonadFail m, MonadResult m)
+  => CmdProgram
+  -> T.Text
+  -> m T.Text
 runCmdProgram prog = runCmdProgramArgs prog []
+
+-- | Runs the command-line program, passing the stream to stdin, and returning a stream from stdout.
+runCmdProgramStream
+  :: (MonadIO m, MonadCatch m, MonadFail m, MonadResult m)
+  => CmdProgram
+  -> S.OutputStream B.ByteString
+  -> m (ResultInputStream T.Text)
+runCmdProgramStream prog = runCmdProgramStreamArgs prog []
